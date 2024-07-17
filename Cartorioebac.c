52a6aa7b49f3579056a376aa7dd5ef0e24591905
Vidoski88 <vitrolla66@gmail.com>
2024-07-16 23:02:51 -0300

    Opção de saida
    Incluso do sistema a opção de saída para o usuário

--- Cartorioebac.c
+++ Cartorioebac.c
@@ -125,6 +125,7 @@
 	    printf("\t1 - Registrar nomes\n");
 	    printf("\t2 - Consultar nomes\n");
  	    printf("\t3 - Deletar nomes\n\n");
+ 	    printf("\t4 - Sair do sistema\n\n");
 	    printf("Opção: "); //fim do menu
 	    
 	    scanf("%d",&opcao);
@@ -146,6 +147,10 @@
 	        	printf("Essa opção não está disponivel!\n");
 	            system("pause");
 	            break;
+	        case 4:
+			    printf("Obrigado por utilizar o sistema!\n"); 
+				return 0;
+				break;    
 	    			
 		}
 	}
